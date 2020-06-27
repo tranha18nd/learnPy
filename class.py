5d45f85e30b28c2f16905df80b7fd3242c700ac8
xDev <tranha18nd@gmail.com>
2020-06-27 08:31:38 +0700

    tao def moi

--- class.py
+++ class.py
@@ -5,6 +5,9 @@ class myClass():
 		self.arg = input("string in here :")
 	def convert(self):
 		print(self.arg.upper())
+	def binhphuong(self):
+		print(int(self.arg)*int(self.arg))
 newClass = myClass()
 newClass.convert()
+newClass.binhphuong()
 		
